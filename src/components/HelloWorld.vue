2f157e403adfbec85c9d5ddd8bb335ffbf997a5b
santy patiño <santyp638@gmail.com>
2020-12-01 23:23:02 -0500

    creadno

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,15 @@
 <template>
   <div class="hello">
-    <!-- <h1>{{ msg }}</h1> -->
-    
-
+    <h1>{{ msg }}</h1>
+  
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  date: {
-      a : "cuadro.png",
-  },
   props: {
     msg: String
   }
 }
 </script>
-
-<style>
-
-
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,40 +8,43 @@
 
         <div class="row m-2 aling-center">
              
-            <div class="c1 col-5">
-                 <img src="comida.jpg" width="120%" height="120%" class="btn"/>
+            <div class="c1 col-5 col-md-3">
+                 <img src="comida.jpg" width="120%" height="120%" class="btn" @click="ingreso()" id="1" />
             </div>
 
-            <div class="c1 col-5" @click="guardar">
-                <img src="frutas.jpg" width="120%" height="120%" class="btn"/>
+            <div class="c1 col-5 col-md-3" @click="guardar">
+                <img src="frutas.jpg" width="120%" height="120%" class="btn" id="2 "/>
             </div>
             
 
-            <div class="c1 col-5">
-                <img src="jugete.jpg" width="120%" height="120%" class="btn"/>
+            <div class="c1 col-5 col-md-3">  
+                <img src="jugete.jpg" width="120%" height="120%" class="btn" id="3" />
             </div>
-            <div class="c1 col-5">
-                <img src="pelicula.jfif" width="120%" height="120%" class="btn"/>
+
+            <div class="c1 col-5 col-md-3">
+                <img src="pelicula.jfif" width="120%" height="120%" class="btn" id="4" />
             </div>
-            <div class="c1 col-5">
-                <img src="anime.jfif" width="120%" height="130%" class="btn"/>
+
+            <div class="c1 col-5 col-md-3">
+                <img src="anime.jfif" width="120%" height="130%" class="btn" id="5" />
             </div>
-            <div class="c1 col-5">
-                <img src="animales.jfif" width="120%" height="130%" class="btn"/>
+
+            <div class="c1 col-5 col-md-3">
+                <img src="animales.jfif" width="120%" height="130%" class="btn" id="6" />
             </div>
            
         </div>
       </div>
     </div>
      
-    <HelloWorld/>
-
+    {{msg="hoal"}}
+<!-- 
       <t v-for="categ in categorias" :key="categ.id" >
             <li class="nav-item active">
-                  <!-- <img src="cuadro.png" width="90%" height="90%"/> -->
+                  <img src="cuadro.png" width="90%" height="90%"/>
            </li>   
       </t>
-      
+       -->
       
   </div>
 </template>
@@ -55,12 +58,22 @@ export default {
    data(){
         return{
             categorias: ["1"],
-            a : "cuadro.png",
     };
    },
   components: {
     HelloWorld
-  }
+  },
+
+  mounted() {
+
+        this.consulta()
+    },
+    methods:{
+
+      consulta(){
+        this.$router.push('/')
+      }
+    }
 }
 </script>
 
@@ -75,10 +88,12 @@ export default {
 .btn{
   border-radius: 20% ;
   border-collapse: inherit;
+  border-block-width: 5px;
+  border-block-color: rgb(17, 186, 216);
   
 }
 .c1{
-  margin-top: 10%;
+  margin-top: 8%;
   margin-right: 8%;
 }
 </style>
